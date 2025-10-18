83833b50f5c3805f85f1e19f8a6cb27ca0bbc1a1
LeksoG <Gagloevi.lekso@gmail.com>
2025-10-18 23:26:46 +0100

    Add EMAIL_GOAL_TEMPLATE_ID to email config
    Added EMAIL_GOAL_TEMPLATE_ID for goal reminders.

--- api/email-config.js
+++ api/email-config.js
@@ -1,7 +1,10 @@
+// api/email-config.js
 export default function handler(req, res) {
   res.status(200).json({
     EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
-    EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID,
+    EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID, // For password reset
+    EMAIL_GOAL_TEMPLATE_ID: process.env.EMAIL_GOAL_TEMPLATE_ID, // NEW: For goal reminders
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY
   });
 }
+
