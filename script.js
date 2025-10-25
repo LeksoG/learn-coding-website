8dd4e6dc52a8518bb9ffeed288ef7c1325819c02
LeksoG <Gagloevi.lekso@gmail.com>
2025-10-25 22:15:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12381,4 +12381,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 setTimeout(() => {
                     isTransitioning = false;
                 }, 300);
+                );
         });
