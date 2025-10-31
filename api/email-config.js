7d1a4a2e80ce3adb9e501c691d00d4db0fe02ce0
LeksoG <Gagloevi.lekso@gmail.com>
2025-10-31 13:20:13 +0000

    Update email-config.js

--- api/email-config.js
+++ api/email-config.js
@@ -3,8 +3,9 @@ export default function handler(req, res) {
   res.status(200).json({
     EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
     EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID, // For password reset
-    EMAIL_GOAL_TEMPLATE_ID: process.env.EMAIL_GOAL_TEMPLATE_ID, // NEW: For goal reminders
+    log_in_verification: process.env.EMAIL_log_in_verification_TEMPLATE_ID, // NEW: For goal reminders
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY
   });
 }
+
 
