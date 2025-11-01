984502f0f5cab0f43593c965f6a8b0c04b1c2aa4
LeksoG <Gagloevi.lekso@gmail.com>
2025-11-01 19:34:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2476,10 +2476,19 @@ function renderChart(selectedLanguage = currentChartLanguage) {
             chartContainer.appendChild(barWrapper);
 
             setTimeout(() => {
-    // 100% = 400px exactly
-    const targetHeight = percentage * 4;
-bar.style.height = `${targetHeight}px`;
-percentageLabel.style.opacity = '1';
+    // Chart usable height: 470px (height) - 10px (top) - 50px (bottom) = 410px
+    // Perfect calculation: 410px / 100 = 4.1px per percentage
+    const targetHeight = percentage * 4.1;
+    bar.style.height = `${targetHeight}px`;
+
+    // Add rounded top for bars under 100%
+    if (percentage < 100) {
+        bar.style.borderRadius = '40px 40px 0px 0px';
+    } else {
+        bar.style.borderRadius = '10px 10px 0px 0px';
+    }
+
+    percentageLabel.style.opacity = '1';
 
                 // Start counting animation after bar starts rising
                 setTimeout(() => {
